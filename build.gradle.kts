4c57d203c0ecccfc13400e393d49d750a851e75c
kunfei <gekunfei@live.com>
2026-05-08 21:34:53 +0800

    优化

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,11 @@ plugins {
 dependencies {
     testImplementation("junit:junit:4.13.2")
 
-    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    // IntelliJ Platform Gradle Plugin Dependencies Extension
+    // read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         intellijIdea("2025.2.6.2")
-        bundledPlugin("com.intellij.java")
+        //bundledPlugin("com.intellij.java")
 
         testFramework(TestFrameworkType.Platform)
     }
